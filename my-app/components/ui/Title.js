1cc4f641456abc8215086f2453a7c064f0cfa13b
akinleyeOJ <akinleyeoj@gmail.com>
2023-03-29 03:27:26 +0200

    chore: update with adding custom fonts and imports

--- my-app/components/ui/Title.js
+++ my-app/components/ui/Title.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+
 import Colors from '../../constants/colors';
 
 function Title({children}) {
@@ -12,8 +13,8 @@ export default Title;
 
 const styles = StyleSheet.create({
     title: {
+      fontFamily: 'open-sans-bold',
       fontSize: 18,
-      fontWeight: 'bold',
       color: 'white',
       textAlign: 'center',
       borderWidth: 2,
